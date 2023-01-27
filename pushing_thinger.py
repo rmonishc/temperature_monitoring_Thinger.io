11467f32d61425e18b9419b8ed90684d083aa5ae
rmonishc <rmonishc@gmail.com>
2023-01-27 19:54:33 +0100

    a way to push temperature and humidity to thinger IO

---
+++ pushing_thinger.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import datetime
+import adafruit_dht
+import board
+from time import sleep
+import requests
+
+#initializing device and reading data
+DHT_device = adafruit_dht.DHT22(board.D17)
+temperature = DHT_device.temperature
+humidity = DHT_device.humidity
+
+#setting frequency to read data
+number_of_seconds_to_sleep = 5
+url = "url given by thinger"
+headers = {'Authorization': 'Bearer '}
+
+
+while True:
+    try:
+        temperature = DHT_device.temperature
+        humidity = DHT_device.humidity
+        time_now = datetime.datetime.now().strftime('%H:%M:%S')
+        # Print what we got to the REPL
+        print("{} Temp: {:.1f} *C \t Humidity: {}%".format(time_now, temperature, humidity))
+        # Prepare the data packet
+        data = {'Place': 'Labo RD', 'Time': time_now, 'Temperature': temperature, 'Humidity': humidity}
+        try:
+            response = requests.post(url, json=data, headers=headers)
+            print(response.status_code)
+        except Exception as e:
+            print(e)
+    except RuntimeError as e:
+        # Reading doesn't always work! Just print error and we'll try again
+        print("Reading from DHT failure: ", e.args)
+    sleep(number_of_seconds_to_sleep)
